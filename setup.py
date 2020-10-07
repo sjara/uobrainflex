cdf78913e1fd098594d6c78212b546d0cd01708a
Santiago Jaramillo <sjara@uoregon.edu>
2020-10-07 09:41:45 -0700

    Created main module files.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uobrainflex',
+    version='0.1dev',
+    author='Santiago Jaramillo',
+    author_email='sjara@uoregon.edu',
+    description='Basic tools for the UO project on brain states and flexible behavior.',
+    packages=find_packages(exclude=[]),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/x-rst; charset=UTF-8',
+    install_requires=['ipython==7.14.0','datajoint>=0.12.5','pynwb>=1.4']
+)
+
+
