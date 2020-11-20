fdc1cef355315c23ad67b69446f533d6b6068291
Santiago Jaramillo <sjara@uoregon.edu>
2020-11-19 23:38:07 -0800

    Fixed missing Anaconda version.

--- scripts/windows_script_launcher.cmd
+++ scripts/windows_script_launcher.cmd
@@ -3,7 +3,7 @@
 :: You can make a copy of this file, modify it, and use it to launch a
 :: script by just double-clicking on the icon.
 
-call C:\Users\%USERNAME%\Anaconda\Scripts\activate.bat
+call C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat
 call conda activate uobrainflex
 call python C:\Users\%USERNAME%\src\uobrainflex\scripts\test_installation.py
 pause
