bb847d3f7dc204d41acdc13d7acb69c29e59d4e6
Santiago Jaramillo <sjara@uoregon.edu>
2020-12-09 16:36:08 -0800

    The script can now read a file listing folders to process.

--- scripts/convert_behavior.py
+++ scripts/convert_behavior.py
@@ -1,16 +1,21 @@
 """
-Converts behavior data to NWB format, copies data to cloud storage,
-and registers the data onto a DataJoint database.
+Convert behavior data to NWB format, copy data to cloud storage,
+and register the data onto a DataJoint database.
 
 Many of these steps require your configuration file (uobrainflex.conf)
 to indicate the correct paths for cloud storage and DataJoint.
 
-How to use this script:
-- Run by specifying the folder containing .txt data files:
-  python convert_behavior.py /data/labview/testmouse_201028_141726/
+Options for how to use this script:
 
-- Run it without arguments to open a GUI to select the data folder:
-  python convert_behavior.py
+1. Run it by specifying the file containing a list of directories to process:
+   python convert_multiple_behavior.py /data/labview/list_of_folders.txt
+
+2. Run it without arguments to open a GUI and select the file that specifies
+   the list of directories to process:
+   python convert_multiple_behavior.py
+
+3. Run it by specifying a single directory to process:
+   python convert_multiple_behavior.py /data/labview/testmouse_201028_141726/
 """
 
 import os
@@ -21,56 +26,69 @@ from uobrainflex import config
 from uobrainflex.nwb import savebehavior
 from uobrainflex.utils import cloudstorage
 # NOTE: Modules related to DataJoint are imported below to avoid
-#       trying to connect to the database unless it's necessary.
+#       trying to connect to the database unless it is necessary.
+#
+# import importlib
+# importlib.reload(savebehavior)  # Useful during development
 
-import importlib
-importlib.reload(savebehavior)  # Useful during development
 
+def read_list_of_folders(txtFile):
+    '''Read list of folders to process from a txt file.'''
+    with open(txtFile,'r') as foldersFile:
+        return [line.strip() for line in foldersFile if line.strip()]
     
-# -- Get input directory --
+# -- Interpret command line arguments --
 if len(sys.argv)>1:
-    inputDir = sys.argv[1]
-else:
+    firstArg = sys.argv[1]
+    if os.path.isdir(firstArg): # Process only the specified directory
+        folders = [firstArg]
+    elif os.path.isfile(firstArg): # Process all directories listed in the specified file
+        folders = read_list_of_folders(firstArg)
+    else:
+        raise ValueError('The first argument needs to be an existing directory or file.')
+else: # Process all directories listed in the file chosen via the GUI
     labview_data_path = config['BEHAVIOR']['labview_data_path']
-    dialogTitle = 'Choose one txt file from the data folder to process'
+    dialogTitle = 'Choose the txt file listing all directories to process'
     rootWin = tkinter.Tk()
     rootWin.withdraw()
-    oneDataFilePath = tkinter.filedialog.askopenfilename(initialdir=labview_data_path,
+    foldersFilename = tkinter.filedialog.askopenfilename(initialdir=labview_data_path,
                                                          title=dialogTitle)
-    if not oneDataFilePath:
+    if not foldersFilename:
         raise ValueError('You need to select a valid file.')
-    inputDir = os.path.split(oneDataFilePath)[0]
-
-# -- Check if output file and folder exist --
-subject = savebehavior.get_subject(inputDir)
-subjectOutputDir = os.path.join(config['BEHAVIOR']['nwb_data_path'], subject)
-nwbFilename = savebehavior.get_nwb_basename(inputDir)
-nwbFullpath = os.path.join(subjectOutputDir,nwbFilename)
-CONVERT_TO_NWB = True
-if not os.path.isdir(subjectOutputDir):
-    print('Created a new folder for this subject: {}'.format(subjectOutputDir))
-    os.mkdir(subjectOutputDir)
-elif os.path.isfile(nwbFullpath):
-    overwriteStr = input('File {} exists. Overwrite? [Y/n] '.format(nwbFullpath))
-    CONVERT_TO_NWB = (overwriteStr.lower()=='y') or not overwriteStr
+    folders = read_list_of_folders(foldersFilename)
 
 # -- Ask user if save and register --
-saveToCloudStr = input('Save data to cloud? [Y/n] ')
+saveToCloudStr = input('Copy data to cloud? [Y/n] ')
 SAVE_TO_CLOUD = (saveToCloudStr.lower()=='y') or not saveToCloudStr
 
 registerStr = input('Register session with DataJoint? [Y/n] ')
 REGISTER_WITH_DATAJOINT = (registerStr.lower()=='y') or not registerStr
 
-if CONVERT_TO_NWB:
-    nwbFile, nwbPath = savebehavior.to_nwb(inputDir, subjectOutputDir)
-    assert nwbPath==nwbFullpath  # Check that it saved in the correct location
+for inputDir in folders:
+    print(f'\nProcessing {inputDir}')
+    # -- Check if output file and folder exist --
+    subject = savebehavior.get_subject(inputDir)
+    subjectOutputDir = os.path.join(config['BEHAVIOR']['nwb_data_path'], subject)
+    nwbFilename = savebehavior.get_nwb_basename(inputDir)
+    nwbFullpath = os.path.join(subjectOutputDir,nwbFilename)
+    CONVERT_TO_NWB = True
+    if not os.path.isdir(subjectOutputDir):
+        print('Created a new folder for this subject: {}'.format(subjectOutputDir))
+        os.mkdir(subjectOutputDir)
+    elif os.path.isfile(nwbFullpath):
+        overwriteStr = input('File {} exists. Overwrite? [Y/n] '.format(nwbFullpath))
+        CONVERT_TO_NWB = (overwriteStr.lower()=='y') or not overwriteStr
 
-if SAVE_TO_CLOUD:
-    cloudPath = cloudstorage.save_behavior_to_cloud(subject, nwbFullpath)
+    if CONVERT_TO_NWB:
+        nwbFile, nwbPath = savebehavior.to_nwb(inputDir, subjectOutputDir)
+        assert nwbPath==nwbFullpath  # Check that it saved in the correct location
 
-if REGISTER_WITH_DATAJOINT:
-    # NOTE: djsumit is imported here to avoid trying to connect to DataJoint before this.
-    from uobrainflex.utils import djtools
-    djBehaviorSession = djtools.submit_behavior_session(nwbFullpath)
+    if SAVE_TO_CLOUD:
+        cloudPath = cloudstorage.save_behavior_to_cloud(subject, nwbFullpath)
+
+    if REGISTER_WITH_DATAJOINT:
+        # NOTE: djsumit is imported here to avoid trying to connect to DataJoint before this.
+        from uobrainflex.utils import djtools
+        djBehaviorSession = djtools.submit_behavior_session(nwbFullpath)
 
 
