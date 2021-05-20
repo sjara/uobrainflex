68bb6bbc4f53597efc662d69b58edaa693a4c2d1
Santiago Jaramillo <sjara@uoregon.edu>
2021-05-20 10:44:57 -0700

    Added template for loadbehavior module.

---
+++ uobrainflex/nwb/loadbehavior.py
@@ -0,0 +1,28 @@
+"""
+Functions and classes for loading behavior data from NWB files.
+"""
+
+import pynwb
+from .. import config
+
+def get_file_path(subject, fileID):
+    """
+    Return full path to an NWB file.
+
+    Args:
+        subject (string): name of subject.
+        fileID (string): ???
+    Returns:
+        nwb_file_path (string): full path to an NWB file
+    """
+    nwb_file_path = ''
+    return nwb_file_path
+
+
+def load_trial_data(nwb_file_path):
+    """
+    """
+    trial_data = []
+    trial_labels = {}
+    return (trial_data, trial_labels)
+
