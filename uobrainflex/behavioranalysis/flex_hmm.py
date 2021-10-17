9879999f360df0783e8f38434d1fd0d271497a47
burnhamdr <burnham1555@gmail.com>
2021-10-16 17:25:20 -0700

    flex_hmm now handles miss trials

--- uobrainflex/behavioranalysis/flex_hmm.py
+++ uobrainflex/behavioranalysis/flex_hmm.py
@@ -7,15 +7,14 @@ Functions for formatting task performance data for use with SSM package
 
 import numpy as np
 
-def format_choice_behavior_hmm(trial_data, trial_labels):
+def format_choice_behavior_hmm(trial_data, trial_labels, drop_no_response=True):
     """
-    
-
     Parameters
     ----------
     trial_data : dataframe
         output of load_trials for a single session.
-
+    drop_no_response : Boolean
+        indicates whether to drop no response trials
     Returns
     -------
     inpts : TYPE
@@ -30,11 +29,12 @@ def format_choice_behavior_hmm(trial_data, trial_labels):
     hit = trial_labels['outcome']['hit']
     error = trial_labels['outcome']['miss']
     
-    # remove no response trials
-
-    target_trials = trial_data.iloc[np.where(trial_data['stimulus_type'] != trial_labels['stimulus_type']['distractor'])[0],:]   
-    response_trials = target_trials.drop(index = target_trials.index[target_trials['outcome']==trial_labels['outcome']['incorrect_reject']].tolist())   
-    response_inds = response_trials.index #get indices of kept trial data
+    if drop_no_response:
+        # remove no response trials
+        response_trials = trial_data.drop(index = trial_data.index[trial_data['outcome']==miss_ind].tolist())
+        response_inds = response_trials.index #get indices of kept trial data
+    else:
+        response_inds = trial_data.index#get ALL indices of trial data
     trial_start = response_trials['start_time'].values
     # create target port ind where left = -1 and right = 1
     target_port = response_trials['target_port']
@@ -43,19 +43,14 @@ def format_choice_behavior_hmm(trial_data, trial_labels):
     target_port = target_port.values
     
     # get coherences of toneclouds presented
-    if trial_data['target_modality'][0] == trial_labels['target_modality']['auditory']:
-        difficulty = response_trials['auditory_stim_difficulty'].values
-        stim = target_port * difficulty
-    else:
-        difficulty = response_trials['visual_gabor_angle'].values
-        stim = (difficulty - 45)/45
-        
+    TC_coherence = response_trials['auditory_stim_difficulty'].values
     
     # multiply coherence by stim id to get inpts variable. this should be shifted to match coherence calculation in the future
+    stim = target_port * TC_coherence
     
     # create true_choice array. This should be created during labview data collection in the future
     true_choice = np.full([len(response_trials),1],np.int(0)) 
-    # step throguh each response and update true_choice
+    # step through each response and update true_choice
     for ind in range(len(response_trials)):
         # if hit and left stim
         if all([response_trials['outcome'].iloc[ind] == hit, response_trials['target_port'].iloc[ind] == left_stim]):
@@ -69,76 +64,10 @@ def format_choice_behavior_hmm(trial_data, trial_labels):
         # if error and left stim
         if all([response_trials['outcome'].iloc[ind] == error, response_trials['target_port'].iloc[ind] == left_stim]):    
             true_choice[ind] = int(1)
+        if response_trials['outcome'].iloc[ind] == miss_ind:#if the outcome is a miss, true choice = 2    
+            true_choice[ind] = int(2)
     
     # create list of choices with [stimulus information, bias constant]
     inpts = [ np.vstack((stim,np.ones(len(stim)))).T ]
     
     return inpts, true_choice, trial_start, response_inds
-
-## test
-## inpts, true_choice = format_behavior_hmm(trial_data)
-## test
-def format_all_behavior_hmm(trial_data, trial_labels):
-    """
-
-    Parameters
-    ----------
-    trial_data : dataframe
-        output of load_trials for a single session.
-
-    Returns
-    -------
-    inpts : TYPE
-        DESCRIPTION.
-    true_choice : TYPE
-        DESCRIPTION.
-
-    """
-
-    right_stim = trial_labels['target_port']['right']
-    left_stim = trial_labels['target_port']['left']
-    hit = trial_labels['outcome']['hit']
-    error = trial_labels['outcome']['miss']
-        
-    # create target port ind where left = -1 and right = 1
-    target_port = trial_data['target_port']
-    target_port = target_port.replace(left_stim,-1)
-    target_port = target_port.replace(right_stim,1)
-    target_port = target_port.values
-    
-    # get coherences of toneclouds presented
-    TC_coherence = trial_data['auditory_stim_difficulty'].values
-    
-    # multiply coherence by stim id to get inpts variable. this should be shifted to match coherence calculation in the future
-    stim = target_port * TC_coherence
-    trial_start = trial_data['start_time'].values
-   
-    # change left stim ID to -1 from 0
-    trial_data['auditory_stim_id'] = trial_data['auditory_stim_id'].replace(0,-1)
-    
-    # multiply coherence by stim id to get inpts variable. this should be shifted to match coherence calculation in the future
-    stim = trial_data['auditory_stim_difficulty'].values*trial_data['auditory_stim_id'].values#*100
-    #stim.astype(int)
-    
-    #default true_choice to 2 (no response)
-    true_choice = np.full([len(trial_data),1],np.int(2)) 
-    # step throguh each response and update true_choice 
-    for ind in range(len(trial_data)):
-        # if hit and left stim
-        if all([trial_data['outcome'].iloc[ind] == hit, trial_data['target_port'].iloc[ind] == left_stim]):
-            true_choice[ind] = int(0)
-        # if error and right stim
-        if all([trial_data['outcome'].iloc[ind] == error, trial_data['target_port'].iloc[ind] == right_stim]):
-            true_choice[ind] = int(0)
-        # if hit and right stim
-        if all([trial_data['outcome'].iloc[ind] == hit, trial_data['target_port'].iloc[ind] == right_stim]):    
-            true_choice[ind] = int(1)
-        # if error and left stim
-        if all([trial_data['outcome'].iloc[ind] == error, trial_data['target_port'].iloc[ind] == left_stim]):    
-            true_choice[ind] = int(1)
-
-
-    
-    inpts = [ np.vstack((stim,np.ones(len(stim)))).T ]
-    
-    return inpts, true_choice, trial_start
